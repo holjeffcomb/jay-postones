7305d99d7d9bfa1da621110d16c5bc1ad9bacfe6
Jeff Holcomb <jeffh.84@gmail.com>
2024-08-13 23:11:53 -0700

    add dynamic sizing to photos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,24 +4,30 @@ import BackgroundVideo from "./_components/BackgroundVideo";
 export default function Home() {
   return (
     <>
-      {/* Full-screen Background Video */}
       <BackgroundVideo />
 
-      {/* Content Below the Video */}
-      <div className="flex flex-row justify-center items-center w-full space-x-4 p-8">
+      <div className="flex flex-row justify-center items-center w-full space-x-4 p-8 min-w-2">
         <Image
           src="/images/drumming.jpg"
           alt="Drumming"
           width={396}
           height={226}
+          className="min-w-72"
         />
         <Image
           src="/images/resources.jpg"
           alt="Drumming"
           width={396}
           height={226}
+          className="min-w-72"
         />
-        <Image src="/images/gear.jpg" alt="Drumming" width={396} height={226} />
+        <Image
+          src="/images/gear.jpg"
+          alt="Drumming"
+          width={396}
+          height={226}
+          className="min-w-72"
+        />
       </div>
     </>
   );
