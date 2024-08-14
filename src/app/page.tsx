bdf2e68f277610d57cd9f9757fd34995d6f565d0
Jeff Holcomb <jeffh.84@gmail.com>
2024-08-14 10:45:32 -0700

    hover and color effects for images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,27 +7,56 @@ export default function Home() {
       <BackgroundVideo />
 
       <div className="flex flex-row justify-center items-center w-full space-x-4 p-8 min-w-2">
-        <Image
-          src="/images/drumming.jpg"
-          alt="Drumming"
-          width={396}
-          height={226}
-          className="min-w-72"
-        />
-        <Image
-          src="/images/resources.jpg"
-          alt="Drumming"
-          width={396}
-          height={226}
-          className="min-w-72"
-        />
-        <Image
-          src="/images/gear.jpg"
-          alt="Drumming"
-          width={396}
-          height={226}
-          className="min-w-72"
-        />
+        <div className="relative min-w-72 rounded-xl overflow-hidden transition-all duration-500 ease-in-out transform hover:scale-105 group">
+          <div className="w-full h-full">
+            <Image
+              src="/images/drumming.jpg"
+              alt="Drumming"
+              width={396}
+              height={226}
+              className="w-full h-full object-cover grayscale group-hover:grayscale-0 transition-all duration-500 ease-in-out"
+            />
+          </div>
+          <div className="absolute inset-0 flex justify-center items-center">
+            <span className="text-white text-4xl font-bold drop-shadow-custom">
+              DRUMMING
+            </span>
+          </div>
+        </div>
+
+        <div className="relative min-w-72 rounded-xl overflow-hidden transition-all duration-500 ease-in-out transform hover:scale-105 group">
+          <div className="w-full h-full">
+            <Image
+              src="/images/resources.jpg"
+              alt="Resources"
+              width={396}
+              height={226}
+              className="w-full h-full object-cover grayscale group-hover:grayscale-0 transition-all duration-500 ease-in-out"
+            />
+          </div>
+          <div className="absolute inset-0 flex justify-center items-center">
+            <span className="text-white text-4xl font-bold drop-shadow-custom">
+              RESOURCES
+            </span>
+          </div>
+        </div>
+
+        <div className="relative min-w-72 rounded-xl overflow-hidden transition-all duration-500 ease-in-out transform hover:scale-105 group">
+          <div className="w-full h-full">
+            <Image
+              src="/images/gear.jpg"
+              alt="Gear"
+              width={396}
+              height={226}
+              className="w-full h-full object-cover grayscale group-hover:grayscale-0 transition-all duration-500 ease-in-out"
+            />
+          </div>
+          <div className="absolute inset-0 flex justify-center items-center">
+            <span className="text-white text-4xl font-bold drop-shadow-custom">
+              GEAR
+            </span>
+          </div>
+        </div>
       </div>
     </>
   );
