0cdbc8230052e4d5a3fd87649a49c56f24bb834c
Jeff Holcomb <jeffh.84@gmail.com>
2024-09-10 10:19:30 -0700

    update sanityClient

--- src/lib/sanityClient.js
+++ src/lib/sanityClient.js
@@ -6,4 +6,5 @@ export const client = createClient({
   apiVersion: "2023-08-30",
   token: process.env.SANITY_API_TOKEN, // Store your token securely in environment variables
   useCdn: false, // `false` if you want to ensure fresh data, `true` for faster responses using cached data
+  withCredentials: true, // This is important for CORS
 });
