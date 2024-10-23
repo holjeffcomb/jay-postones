df4e99c49e0076399d5391b3b1a12b64f0f17532
Jeff Holcomb <jeffh.84@gmail.com>
2024-10-23 16:05:59 -0700

    add flexibility to grid height

--- src/app/lessons/_components/LessonGrid.tsx
+++ src/app/lessons/_components/LessonGrid.tsx
@@ -6,7 +6,7 @@ export default function LessonGrid({ lessons }: { lessons: any[] }) {
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-6">
       {lessons.map((course) => (
         <Link href={`/lessons/${course._id}`} key={course._id}>
-          <div className="flex flex-col bg-[var(--secondary-color)] rounded-lg shadow-[4px_4px_4px_rgba(0,0,0,0.8)] overflow-hidden transition-all duration-300 ease-in-out hover:scale-[1.02] hover:shadow-lg hover:bg-[#3D3F5D] group min-w-[300px] w-full h-[460px]">
+          <div className="flex flex-col bg-[var(--secondary-color)] rounded-lg shadow-[4px_4px_4px_rgba(0,0,0,0.8)] transition-all duration-300 ease-in-out hover:scale-[1.02] hover:shadow-lg hover:bg-[#3D3F5D] group min-w-[300px] w-full min-h-[460px] max-h-[none] overflow-visible">
             {course.imageUrl && (
               <div className="relative w-full pt-[56.25%]">
                 <Image
@@ -17,7 +17,17 @@ export default function LessonGrid({ lessons }: { lessons: any[] }) {
                   className="absolute top-0 left-0 w-full h-full"
                 />
                 <div
-                  className={`absolute top-0 right-0 px-2 rounded-sm m-2 text-[var(--text-color)] ${course.membershipLevel === "free" ? "bg-blue-500" : course.membershipLevel === "silver" ? "bg-gray-500" : course.membershipLevel === "gold" ? "bg-yellow-500" : course.membershipLevel === "platinum" ? "bg-purple-500" : "bg-[var(--accent-color)]"}`}
+                  className={`absolute top-0 right-0 px-2 rounded-sm m-2 text-[var(--text-color)] ${
+                    course.membershipLevel === "free"
+                      ? "bg-blue-500"
+                      : course.membershipLevel === "silver"
+                        ? "bg-gray-500"
+                        : course.membershipLevel === "gold"
+                          ? "bg-yellow-500"
+                          : course.membershipLevel === "platinum"
+                            ? "bg-purple-500"
+                            : "bg-[var(--accent-color)]"
+                  }`}
                 >
                   <span className="text-sm font-normal">
                     {course.membershipLevel?.toUpperCase()}
@@ -36,7 +46,13 @@ export default function LessonGrid({ lessons }: { lessons: any[] }) {
             )}
             <div className="p-3 flex flex-col gap-1 flex-grow">
               <span
-                className={`text-sm font-normal ${course.level === "beginner" ? "text-green-500" : course.level === "intermediate" ? "text-yellow-500" : "text-red-500"}`}
+                className={`text-sm font-normal ${
+                  course.level === "beginner"
+                    ? "text-green-500"
+                    : course.level === "intermediate"
+                      ? "text-yellow-500"
+                      : "text-red-500"
+                }`}
               >
                 {course.level}
               </span>
