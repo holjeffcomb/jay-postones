f1a62272d749b0dede1fd1a16f46800057a8401a
Jeff Holcomb <jeffh.84@gmail.com>
2024-12-26 22:31:27 -0500

    update form styling, handle user already exists, password mismatch

--- src/app/login/actions.ts
+++ src/app/login/actions.ts
@@ -28,19 +28,37 @@ export async function login(formData: FormData) {
 export async function signup(formData: FormData) {
   const supabase = await createClient();
 
-  // type-casting here for convenience
-  // in practice, you should validate your inputs
-  const data = {
-    email: formData.get("email") as string,
-    password: formData.get("password") as string,
-  };
+  // Prepare data
+  const email = formData.get("email") as string;
+  const password = formData.get("password") as string;
+  const firstName = formData.get("firstName") as string;
+  const lastName = formData.get("lastName") as string;
 
-  const { error } = await supabase.auth.signUp(data);
+  // Attempt to sign up the user
+  const { data, error } = await supabase.auth.signUp({
+    email,
+    password,
+    options: {
+      data: {
+        first_name: firstName,
+        last_name: lastName,
+      },
+    },
+  });
 
-  if (error) {
-    redirect("/error");
-  }
+  let authError: { name: string; message: string } | null = null;
 
-  revalidatePath("/", "layout");
-  redirect("/");
+  // User exists, but is fake. See https://supabase.com/docs/reference/javascript/auth-signup
+  if (data.user && data.user.identities && data.user.identities.length === 0) {
+    authError = {
+      name: "AuthApiError",
+      message: "User already exists",
+    };
+  } else if (error)
+    authError = {
+      name: error.name,
+      message: error.message,
+    };
+
+  return { auth: data, error: authError };
 }
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,13 +2,68 @@ import { login, signup } from "./actions";
 
 export default function LoginPage() {
   return (
-    <form className="flex flex-col w-64 m-auto">
-      <label htmlFor="email">Email:</label>
-      <input id="email" name="email" type="email" required />
-      <label htmlFor="password">Password:</label>
-      <input id="password" name="password" type="password" required />
-      <button formAction={login}>Log in</button>
-      <button formAction={signup}>Sign up</button>
-    </form>
+    <div className="flex flex-col items-center justify-center bg-[var(--primary-color)]">
+      <div className="bg-gray-800 text-white shadow-lg rounded-lg w-[500px] p-8">
+        <h2 className="text-2xl font-semibold text-center mb-6">
+          Welcome Back
+        </h2>
+        <form className="flex flex-col gap-4">
+          <div className="flex flex-col">
+            <label
+              htmlFor="email"
+              className="text-sm font-medium text-gray-300 mb-2"
+            >
+              Email Address
+            </label>
+            <input
+              id="email"
+              name="email"
+              type="email"
+              required
+              className="bg-gray-700 text-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+              placeholder="Enter your email"
+            />
+          </div>
+          <div className="flex flex-col">
+            <label
+              htmlFor="password"
+              className="text-sm font-medium text-gray-300 mb-2"
+            >
+              Password
+            </label>
+            <input
+              id="password"
+              name="password"
+              type="password"
+              required
+              className="bg-gray-700 text-gray-300 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+              placeholder="Enter your password"
+            />
+          </div>
+          <div className="flex flex-col justify-between items-center gap-5">
+            <button
+              formAction={login}
+              className="bg-blue-600 hover:bg-blue-500 text-white font-semibold py-2 px-4 w-full rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
+            >
+              Log In
+            </button>
+            {/* <button
+              formAction={signup}
+              className="bg-green-600 hover:bg-green-500 text-white font-semibold py-2 px-4 rounded-lg focus:outline-none focus:ring-2 focus:ring-green-400"
+            >
+              Sign Up
+            </button>
+          </div>
+          <div className="mt-4 text-center"> */}
+            <a
+              href="/register"
+              className="text-blue-400 hover:text-blue-300 text-sm"
+            >
+              Not a member? Register here.
+            </a>
+          </div>
+        </form>
+      </div>
+    </div>
   );
 }
