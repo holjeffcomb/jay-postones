a44fc9d6f75f93203b0319fd46bf2495ae47af59
Jeff Holcomb <jeffh.84@gmail.com>
2025-03-02 16:47:13 -0800

    change ss to height 700px

--- src/app/lessons/[id]/lessonContext.tsx
+++ src/app/lessons/[id]/lessonContext.tsx
@@ -165,7 +165,7 @@ export const LessonProvider = ({ children, lessonId }: LessonProviderProps) => {
         <iframe
           src={soundsliceUrlWithUser}
           width="100%"
-          height="100%"
+          height="700"
           allowFullScreen
         ></iframe>
       );
